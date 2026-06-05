8629e12dda6c223db319ea414ed31346ee9f9915
giorgi-bezhiashvili <giorgibezhiashvili5@gmail.com>
2026-06-05 09:10:01 -0700

    Created A home page

---
+++ Newton/src/App.tsx
@@ -0,0 +1,11 @@
+import Header from "./header"
+import Middle from "./middle"
+import Footer from "./footer"
+function App() {
+  return <><Header/>
+  <Middle/>
+  <Footer/>
+  </>
+}
+
+export default App
---
+++ Newton/src/footer.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return(
+        <footer>
+            <div className="footerContainer">
+                <p> 2026 Newton©. ყველა უფლება დაცულია.
+
+                ინოვაციური პლატფორმა ფიზიკის შესასწავლად. ისწავლე, ივარჯიშე და აღმოაჩინე სამყარო ჩვენთან ერთად.</p>
+                <a href="https://www.linkedin.com/in/giorgi-bezhiashvili-b8377033b/"><span>Developer : Giorgi-Bezhiashvili</span></a>
+            </div>
+        </footer>
+    )
+}
+export default Footer
